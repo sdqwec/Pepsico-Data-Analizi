ee7b40983dddef1d26a5f3e01b35dfab9bffa186
Evren Emrekan Aydın <109812692+sdqwec@users.noreply.github.com>
2024-09-04 02:08:30 +0300

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 00:53:25 2024
+
+@author: eaydin
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+veri = pd.read_csv("PEP.csv",)
+fiyatlar = veri[['Open','Close']]
+veri["YEARS"]= pd.to_datetime(veri['Date'])
+plt.plot( veri["YEARS"],fiyatlar)
+plt.xlabel("Years")
+plt.ylabel("Dollars")
+plt.title("Open and Closes of Pepsico")
+plt.show()
+
